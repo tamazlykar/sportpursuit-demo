770989ba82248658ef0c460407adfd5f1c61a7d8
Tamazlykar Daniil <tadani7@gmail.com>
2017-12-25 03:40:11 +0700

    Code review and removing unnecessary code

--- app/home/home.component.ts
+++ app/home/home.component.ts
@@ -1,9 +1,6 @@
 import { Component, OnInit, ViewChild } from "@angular/core";
-import { DrawerTransitionBase, SlideInOnTopTransition } from "nativescript-pro-ui/sidedrawer";
 import { RadSideDrawerComponent } from "nativescript-pro-ui/sidedrawer/angular";
 import { HomeService } from './home.service';
-import { ScrollView, ScrollEventData } from "ui/scroll-view";
-import { EventData } from "tns-core-modules/data/observable/observable";
 import { ITopSalesBanner, IFlashSaleItem, IBestseller } from './models';
 import { login, LoginResult } from "ui/dialogs";
 
@@ -18,26 +15,15 @@ export class HomeComponent implements OnInit {
   public flashSales: IFlashSaleItem[];
 
   @ViewChild("drawer") private drawerComponent: RadSideDrawerComponent;
-  private _sideDrawerTransition: DrawerTransitionBase;
 
   constructor(private homeService: HomeService) {}
 
   public ngOnInit(): void {
-    this._sideDrawerTransition = new SlideInOnTopTransition();
     this.loadData();
-  }
-
-  get sideDrawerTransition(): DrawerTransitionBase {
-    return this._sideDrawerTransition;
   }
 
   public onDrawerButtonTap(): void {
     this.drawerComponent.sideDrawer.showDrawer();
-  }
-
-  public onTap(args: EventData) {
-    // using the unique id assigned via the view-model
-    console.log(args.object.get("id"));
   }
 
   public onTapAuth() {
@@ -49,16 +35,6 @@ export class HomeComponent implements OnInit {
     };
 
     login(options);
-  }
-
-  public onScroll(args: ScrollEventData) {
-      console.log("scrollX: " + args.scrollX + "; scrollY: " + args.scrollY);
-  }
-
-  public onScrollLoaded(args) {
-      // scroll to specific position of the horizontal scroll list
-      const scrollOffset = 330;
-      (<ScrollView>args.object).scrollToHorizontalOffset(scrollOffset, true);
   }
 
   private loadData() {
