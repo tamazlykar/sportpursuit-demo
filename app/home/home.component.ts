633f535b441e17c4024d5f3f006eb370140e27e6
Tamazlykar Daniil <tadani7@gmail.com>
2017-12-25 04:44:13 +0700

    Add activity indicator for home page loading data

--- app/home/home.component.ts
+++ app/home/home.component.ts
@@ -3,16 +3,20 @@ import { RadSideDrawerComponent } from "nativescript-pro-ui/sidedrawer/angular";
 import { HomeService } from './home.service';
 import { ITopSalesBanner, IFlashSaleItem, IBestseller } from './models';
 import { login, LoginResult } from "ui/dialogs";
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable//forkJoin';
 
 @Component({
     selector: "Home",
     moduleId: module.id,
-    templateUrl: "./home.component.html"
+    templateUrl: "./home.component.html",
+    styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
   public topSalesBanners: ITopSalesBanner[];
   public bestsellers: IBestseller[];
   public flashSales: IFlashSaleItem[];
+  public isLoading = true;
 
   @ViewChild("drawer") private drawerComponent: RadSideDrawerComponent;
 
@@ -38,11 +42,27 @@ export class HomeComponent implements OnInit {
   }
 
   private loadData() {
-    this.homeService.getTopSalesBanners()
-      .subscribe(data => this.topSalesBanners = data);
-    this.homeService.getBestsellers()
-      .subscribe(data => this.bestsellers = data);
-    this.homeService.getFlashSales()
-      .subscribe(data => this.flashSales = data);
+    const tasks = [];
+    tasks.push(this.homeService.getTopSalesBanners());
+    tasks.push(this.homeService.getBestsellers());
+    tasks.push(this.homeService.getFlashSales());
+
+    Observable.forkJoin(...tasks)
+      .subscribe((data: Array<any>) => {
+        this.topSalesBanners = data[0];
+        this.bestsellers = data[1];
+        this.flashSales = data[2];
+        this.isLoading = false;
+      });
   }
+
+  // private loadData() {
+  //   const tasks = [];
+  //   this.homeService.getTopSalesBanners()
+  //     .subscribe(data => this.topSalesBanners = data);
+  //   this.homeService.getBestsellers()
+  //     .subscribe(data => this.bestsellers = data);
+  //   this.homeService.getFlashSales()
+  //     .subscribe(data => this.flashSales = data);
+  // }
 }
