31a564e7ce153750f9d2f2c069568a8c2def69ec
Tamazlykar Daniil <tadani7@gmail.com>
2017-12-30 23:46:22 +0500

    Fix bug with Android WebView

--- app/web-view/web-view.component.ts
+++ app/web-view/web-view.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 import { PageRoute, RouterExtensions } from "nativescript-angular/router";
+import { WebView } from "ui/web-view";
 import "rxjs/add/operator/switchMap";
 
 import { URL } from '../shared/config';
@@ -9,9 +10,10 @@ import { URL } from '../shared/config';
   moduleId: module.id,
   templateUrl: './web-view.component.html'
 })
-export class WebViewComponent implements OnInit {
+export class WebViewComponent implements OnInit, AfterViewInit {
   public url = URL;
   public link: string = '';
+  @ViewChild("myWebView") public webViewRef: ElementRef;
 
   constructor(private pageRoute: PageRoute, private routerExtensions: RouterExtensions) {}
 
@@ -19,8 +21,19 @@ export class WebViewComponent implements OnInit {
     this.pageRoute.activatedRoute
       .switchMap(activatedRoute => activatedRoute.params)
       .forEach(params => this.link = params['url']);
-
   }
+
+  public ngAfterViewInit() {
+    const webview: WebView = this.webViewRef.nativeElement;
+
+    webview.on(WebView.loadStartedEvent, () => {
+      // "setDomStorageEnabled" needs to be set for correct website loading
+      // Othervise you will get Errors and some js scripts won't be executed inside WebView
+      if (webview.android) {
+        webview.android.getSettings().setDomStorageEnabled(true);
+      }
+    });
+}
 
   public onNavBtnTap() {
     this.routerExtensions.backToPreviousPage();
